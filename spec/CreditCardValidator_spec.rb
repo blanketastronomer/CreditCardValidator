874677f1f4c23b5b23066b3f3476344e1f82871b
blanketastronomer <blanketastronomer@users.noreply.github.com>
2017-11-13 08:08:21 -0800

    Remove useless test

--- spec/CreditCardValidator_spec.rb
+++ spec/CreditCardValidator_spec.rb
@@ -4,8 +4,4 @@ RSpec.describe CreditCardValidator do
   it "has a version number" do
     expect(CreditCardValidator::VERSION).not_to be nil
   end
-
-  it "does something useful" do
-    expect(false).to eq(true)
-  end
 end
